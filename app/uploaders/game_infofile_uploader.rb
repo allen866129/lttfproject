9972998cc12c141c6af4b2b24f46c5826364beb4
Kunchan Lee <allen866129@gmail.com>
2016-05-21 23:19:16 +0800

    for rails-4-2-6 version

--- app/uploaders/game_infofile_uploader.rb
+++ app/uploaders/game_infofile_uploader.rb
@@ -46,9 +46,13 @@ class GameInfofileUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-    #include Sprockets::Helpers::RailsHelper
-    #include Sprockets::Helpers::IsolatedHelper
-    include Sprockets::Rails::Helper
+  if Rails::VERSION::MAJOR >= 4
+     include Sprockets::Rails::Helper
+  else
+    include Sprockets::Helpers::RailsHelper
+    include Sprockets::Helpers::IsolatedHelper
+  # Do it the old way
+   end
   # Provide a default URL as a default if there hasn't been a file uploaded:
    def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
