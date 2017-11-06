a07b22a3d770e5a1f8c23aaa788f9c634d988003
Kunchan Lee <allen866129@gmail.com>
2017-11-06 16:45:26 +0800

    show stars in gamegroup

--- app/models/attendant.rb
+++ app/models/attendant.rb
@@ -3,4 +3,11 @@ class Attendant < ActiveRecord::Base
   attr_accessible :player_id
   default_scope {order('created_at ASC')}
   belongs_to :groupattendant
+
+def stars_picture
+
+	user=User.find(self.player_id)
+	#@user.rating_stars_picture
+	return user.rating_stars_picture
+end	
 end
