b9d5836f64b8b4614745246553fe9f19573d7ca5
allen866129 <allen866129@users.noreply.github.com>
2019-05-16 21:28:25 +0800

    fix strong parameters issue for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
- 
+ before_filter :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
  protect_from_forgery with: :exception
@@ -16,5 +16,9 @@ def  find_admins_superusers
     admins_superusers=(admins+superusers).uniq
     admins_superusers
 end
+def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:account_update) { |u| u.permit( :id, :playerphoto, :fbaccount, :phone,:email, :password, :password_confirmation, :current_password, 
+playerprofile_attributes: [ :id, :name, :initscore, :curscore, :totalwongames,  :totallosegames, :lastgamedate, :lastgamename, :lastscoreupdatedate, :gamehistory, :profileurl, :imageurl, :bio, :paddleholdtype, :paddlemodel, :forwardrubber, :backrubber  , :created_at , :updated_at ]) }
 
+end
 end
